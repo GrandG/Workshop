9638988e14ef8321201491d23ec2b3b093726918
GrandG <616311401@qq.com>
2017-05-15 09:12:00 +0800

    first version

--- featech/CurveFitting/curve_fitting.py
+++ featech/CurveFitting/curve_fitting.py
@@ -4,15 +4,20 @@ from scipy.optimize import curve_fit
 from functions import efunc
 from functions import lfunc
 from functions import pfunc
-# import warnings
-# np.seterr(all='warn')
+import warnings
 
 
 class CurveFitting:
+    """曲线拟合"""
     def __init__(self, points):
-        self._points = points
-        self._xdata = np.array([i[0] for i in sorted(self._points, key=lambda x: x[0])])
-        self._ydata = np.array([i[1] for i in sorted(self._points, key=lambda x: x[0])])
+        """对输入坐标进行矩阵化处理，并按横坐标排序"""
+        try:
+            self._points = points
+            self._xdata = np.array([i[0] for i in sorted(self._points, key=lambda x: x[0])])
+            self._ydata = np.array([i[1] for i in sorted(self._points, key=lambda x: x[0])])
+        except TypeError:
+            print('输入错误的坐标')
+
         self._xfit = 0
         self._yfit = 0
         self._n = 0
@@ -20,70 +25,127 @@ class CurveFitting:
         self._pcov = None
 
     def poly(self, xfit, degree=3):
-        self._xfit = xfit
-        z = np.polyfit(self._xdata, self._ydata, degree)
-        p = np.poly1d(z)
-        return p(self._xfit)
+        """多项式拟合，默认最高阶是3"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            try:
+                self._xfit = xfit
+                z = np.polyfit(self._xdata, self._ydata, degree)
+                p = np.poly1d(z)
+                return p(self._xfit)
+            except np.RankWarning:
+                print('多项式拟合效果差')
+            except TypeError:
+                print('多项式拟合失败')
+                return False
 
     def poly_show(self, n=100, degree=3):
+        """绘图"""
         try:
             self._n = n
             plt.plot(self._xdata, self._ydata, 'ro', label='true')
             plt.plot(np.linspace(self._xdata[0], self._xdata[-1],
-                                 self._n), self.poly(np.linspace(self._xdata[0],
-                                                                 self._xdata[-1], self._n),
-                                                     degree=degree), label='poly fitting')
+                                 self._n), self.poly(np.linspace(self._xdata[0], self._xdata[-1],
+                                                                 self._n), degree=degree), label='poly fitting')
             plt.xlabel("x")
-            plt.ylabel("y")
+            plt.ylabel("y", rotation='horizontal')
+            plt.grid(True)
             plt.legend()
             plt.show()
-        except Warning as w:
-            print('Now is {}'.format(w))
+        except ValueError:
+            print('绘图失败')
+            return False
+        except AttributeError:
+            print('绘图失败')
+            return False
 
     def exp(self, xfit=0):
-        self._xfit = xfit
-        self._popt, self._pcov = curve_fit(efunc, self._xdata, self._ydata)
-        # return efunc(self._xfit, *self._popt)
+        """指数拟合"""
+        try:
+            self._xfit = xfit
+            self._popt, self._pcov = curve_fit(efunc, self._xdata, self._ydata)
+            return efunc(self._xfit, *self._popt)
+        except AttributeError:
+            print('指数拟合失败')
+            return False
 
     def exp_show(self, n=100):
-        self._n = n
-        self.exp()
-        plt.plot(self._xdata, self._ydata, 'ro', label='true')
-        plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n), efunc(np.linspace(self._xdata[0],
-                 self._xdata[-1], self._n), *self._popt), label='exp fitting')
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend()
-        plt.show()
+        """绘图"""
+        try:
+            self._n = n
+            # self.exp()
+            plt.plot(self._xdata, self._ydata, 'ro', label='true')
+            plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n),
+                     self.exp(np.linspace(self._xdata[0], self._xdata[-1], self._n)), label='exp fitting')
+            plt.xlabel("x")
+            plt.ylabel("y", rotation='horizontal')
+            plt.grid(True)
+            plt.legend()
+            plt.show()
+        except AttributeError:
+            print('绘图失败')
+            return False
 
-    def loga(self, xfit=0):
-        self._xfit = xfit
-        self._popt, self._pcov = curve_fit(lfunc, self._xdata, self._ydata)
+    def loga(self, xfit=1):
+        """对数拟合，输入横坐标要求是正数"""
+        with warnings.catch_warnings():
+            warnings.filterwarnings('error')
+            try:
+                self._xfit = xfit
+                self._popt, self._pcov = curve_fit(lfunc, self._xdata, self._ydata)
+                return lfunc(self._xfit, *self._popt)
+            except RuntimeWarning:
+                print('输入横坐标必须为正数')
+                return False
+            except AttributeError:
+                print('对数拟合失败')
+                return False
+
 
     def loga_show(self, n=100):
-        self._n = n
-        self.loga()
-        plt.plot(self._xdata, self._ydata, 'ro', label='true')
-        plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n),
-                 lfunc(np.linspace(self._xdata[0], self._xdata[-1], self._n), *self._popt), label='loga fitting')
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend()
-        plt.show()
+        """绘图"""
+        try:
+            self._n = n
+            plt.plot(self._xdata, self._ydata, 'ro', label='true')
+            plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n),
+                     self.loga(np.linspace(self._xdata[0], self._xdata[-1], self._n)), label='loga fitting')
+            plt.xlabel("x")
+            plt.ylabel("y", rotation='horizontal')
+            plt.grid(True)
+            plt.legend()
+            plt.show()
+        except ValueError:
+            print('绘图失败')
+            return False
+        except AttributeError:
+            print('绘图失败')
+            return False
 
     def power(self, xfit=0):
-        self._xfit = xfit
-        self._popt, self._pcov = curve_fit(pfunc, self._xdata, self._ydata)
+        """幂函数拟合"""
+        try:
+            self._xfit = xfit
+            self._popt, self._pcov = curve_fit(pfunc, self._xdata, self._ydata)
+            return pfunc(self._xfit, *self._popt)
+        except AttributeError:
+            print('幂函数拟合失败')
+            return False
 
     def power_show(self, n=100):
-        self._n = n
-        self.power()
-        plt.plot(self._xdata, self._ydata, 'ro', label='true')
-        plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n),
-                 pfunc(np.linspace(self._xdata[0], self._xdata[-1], self._n), *self._popt), label='power fitting')
-        plt.xlabel("x")
-        plt.ylabel("y")
-        plt.legend()
-        plt.show()
-        plt.show()
+        """绘图"""
+        try:
+            self._n = n
+            self.power()
+            plt.plot(self._xdata, self._ydata, 'ro', label='true')
+            plt.plot(np.linspace(self._xdata[0], self._xdata[-1], self._n),
+                     self.power(np.linspace(self._xdata[0], self._xdata[-1], self._n)), label='power fitting')
+            plt.xlabel("x")
+            plt.ylabel("y", rotation='horizontal')
+            plt.grid(True)
+            plt.legend()
+            plt.show()
+        except AttributeError:
+            print('绘图失败')
+            return False
+
 
--- featech/CurveFitting/test.py
+++ featech/CurveFitting/test.py
@@ -4,16 +4,16 @@ import numpy as np
 
 
 if __name__ == '__main__':
-    test0 = CurveFitting(pdata())    # 多项式拟合
-    test0.poly_show(degree=10)
+    test0 = CurveFitting(ldata())    # 多项式拟合
+    test0.poly_show(degree=9)
 
-    test1 = CurveFitting(edata())    # 指数拟合
-    test1.exp_show()
-
-    test2 = CurveFitting(ldata())    # 对数拟合
-    test2.loga_show()
-
-    test3 = CurveFitting(pdata())    # 幂函数拟合
-    test3.power_show()
+    # test1 = CurveFitting(edata())    # 指数拟合
+    # test1.exp_show()
+    #
+    # test2 = CurveFitting(ldata())    # 对数拟合
+    # test2.loga_show()
+    #
+    # test3 = CurveFitting(pdata())    # 幂函数拟合
+    # test3.power_show()
 
 
